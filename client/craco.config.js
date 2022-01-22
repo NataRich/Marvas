e8720857a8ccff0cad50922c7e8f3e24b2e48417
Jaylan Zhao <natarichjay@gmail.com>
2022-01-22 14:59:43 -0500

    Configure antd & tailwindcss

--- client/craco.config.js
+++ client/craco.config.js
@@ -7,8 +7,9 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
+            // Refer to docs - https://ant.design/docs/react/customize-theme-cn
             modifyVars: { 
-                '@primary-color': '#0D9488' 
+                '@primary-color': '#0D9488'                
                 },
             javascriptEnabled: true,
           },
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: colors.teal,
+      neutral: colors.neutral,
+      white: "#FFFFFF"
+    },
+    fontFamily: {
+      sans: ['Roboto']
+    }
   },
   plugins: [],
 }
