6de2d38040df171e25b26e4207e83f39c6e326d9
Jaylan Zhao <natarichjay@gmail.com>
2022-01-22 15:38:56 -0500

    Configure routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
 import './App.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/mall" element={<Mall />} />
+          <Route path="/item/:id" element={<ProductPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} /> */}
+        </Routes>
+      </Router>
+      {/* <Footer /> */}
     </div>
   );
 }
