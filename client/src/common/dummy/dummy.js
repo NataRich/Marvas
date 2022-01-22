a10f7efac35643a1a33516197bc9b76a3bcdc0ad
Jaylan Zhao <natarichjay@gmail.com>
2022-01-22 17:35:02 -0500

    Add dummy data

---
+++ client/src/common/dummy/dummy.js
@@ -0,0 +1,15 @@
+export const categories = [
+    'Sports',
+    'Electronics',
+    'Clothes',
+    'Books',
+    'Health',
+    'Toys',
+    'Handmade'
+]
+
+export const emails = [
+    'contact@marvas.com',
+    'support@marvas.com',
+    'work@marvas.com'
+]
