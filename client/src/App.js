83183eaf763d0d191aab017fb95f86c375ce2ef1
Yuehan Wang <yuehan@cs.uchicago.edu>
2022-01-22 22:05:08 -0600

    signin&signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Signin from './pages/signin/Signin';
+import Signup from './pages/signup/Signup';
 import Header from './common/header/Header'
 import Footer from './common/footer/Footer';
 import Product from './common/product/Product';
@@ -13,12 +15,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Product" element={<Product />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/signin" element={<Signin />} /> 
+          <Route path="/signup" element={<Signup />} /> 
           {/* <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/item/:id" element={<ProductPage />} />
           <Route path="/list" element={<ListPage />}
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<Signin />} /> */}
+          */}
         </Routes>
         
         <Footer />
