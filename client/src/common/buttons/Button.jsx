4d4fd1d22eef3aca8f27962b1c53fe740b78f680
Jaylan Zhao <natarichjay@gmail.com>
2022-01-22 20:20:56 -0500

    Add more effects

--- client/src/common/buttons/Button.jsx
+++ client/src/common/buttons/Button.jsx
@@ -4,9 +4,9 @@ export function SignInButton() {
     const navigate = useNavigate();
 
     return (
-        <div className="border border-primary-600 hover:bg-primary-600 focus:bg-primary-800 group">
+        <div className="border rounded-[2px] border-primary-600 hover:bg-primary-600 active:!bg-primary-700 active:shadow-inner active:border-primary-700 transition-all group">
             <button 
-                className="font-sans text-xl text-primary-600 group-hover:text-white py-[10px] px-[20px]"
+                className="font-sans text-xl text-primary-600 group-hover:text-white active:text-white transition-all py-[10px] px-[20px]"
                 onClick={() => {navigate('/signin')}}
             >
                 Sign In
