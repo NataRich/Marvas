4bb7517ced672cafcb94b52c42c34947bb1cd283
Jaylan Zhao <natarichjay@gmail.com>
2022-01-23 01:00:01 -0500

    Add search redirection

--- client/src/common/search/SearchBar.jsx
+++ client/src/common/search/SearchBar.jsx
@@ -1,17 +1,21 @@
 import { Input } from "antd";
 import { AudioOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
 import "./custom.css"
 
 const { Search } = Input;
 
 export default function SearchBar() {
+    const navigate = useNavigate();
+
     const handleRecord = () => {
         console.log("recording...")
     }
 
-    const handleSearch = (value) => {
-        console.log(value)
+    const handleSearch = (value, _) => {
+        if (value)
+            navigate(`/marketplace?query=${value}`)
     }
 
     const suffix = (
